3a07151134274def154b5db353594ec61b420b0a
Sam Verschueren <sam.verschueren@gmail.com>
2015-09-03 07:26:26 +0200

    Added support for aws-lambda-env

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 
 /**
  * Yeoman generator for AWS Lambda functions in NodeJS.
- * 
+ *
  * @author Sam Verschueren      <sam.verschueren@gmail.com>
  * @since  21 Aug. 2015
  */
@@ -16,7 +16,7 @@ var path = require('path'),
 module.exports = yeoman.generators.Base.extend({
     init: function() {
         var done = this.async();
-        
+
         // Ask the questions
         this.prompt([
             {
@@ -51,8 +51,19 @@ module.exports = yeoman.generators.Base.extend({
                 message: 'Do you need want to invoke other lambda functions?',
                 type: 'confirm',
                 default: true
+            },
+            {
+                name: 'env',
+                message: 'Do you need want to extract the environment from the function name?',
+                type: 'confirm',
+                default: true
             }
         ], function(props) {
+            // Build the list of dependencies
+            var dependencies = {};
+            if(props.invoke)    dependencies['aws-lambda-invoke'] = '^1.0.4';
+            if(props.env)       dependencies['aws-lambda-env'] = '^1.0.2';
+
             // Build up the template
             var tpl = {
                 functionName: props.functionName,
@@ -61,20 +72,22 @@ module.exports = yeoman.generators.Base.extend({
                 name: this.user.git.name(),
                 email: this.user.git.email(),
                 invoke: props.invoke,
+                env: props.env,
+                dependencies: dependencies,
                 date: moment().format('DD MMM. YYYY')
             };
-            
+
             var mv = function (from, to) {
                 this.fs.move(this.destinationPath(from), this.destinationPath(to));
             }.bind(this);
-            
+
             // Copy the template files
             this.fs.copyTpl(this.templatePath() + '/**', this.destinationPath(), tpl);
-            
+
             // Rename the files
             mv('_package.json', 'package.json');
             mv('gitignore', '.gitignore');
-            
+
             // We are done!
             done();
         }.bind(this));
--- app/templates/index.js
+++ app/templates/index.js
@@ -2,18 +2,19 @@
 
 /**
  * <%= functionDescription %>
- * 
+ *
  * @author <%= name %>      <<%= email %>>
  * @since  <%= date %>
  */
 
 // module dependencies
 var AWS = require('aws-sdk')<% if (invoke) { %>,
-    lambda = require('aws-lambda-invoke')(AWS)<% } %>;
+    lambda = require('aws-lambda-invoke')(AWS)<% } %><% if (env) { %>,
+    env = require('aws-lambda-env')() || 'production'<% } %>;
 
 /**
  * The handler function.
- * 
+ *
  * @param {object}  event       The data regarding the event.
  * @param {object}  context     The AWS Lambda execution context.
  */
